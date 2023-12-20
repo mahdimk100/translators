037f44f03271ab1dce6d4ef1d6ca8348fd7d0661
mahdi khodaei <109039802+mahdimk100@users.noreply.github.com>
2023-12-20 21:57:30 +0330

    views def home

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from translate import Translator
 
 # Create your views here.
+
+def home(request):
+    if request.method == 'POST':
+        text = request.POST['translate']
+        language = request.POST['language']
+        translator = Translator(to_lang=language)
+        tranlation = translator.translate(text)
+        return HttpResponse(tranlation)
+    return render(request, 'main/index.html')
